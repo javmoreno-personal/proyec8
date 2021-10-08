d64f5d1816f220b903e2a2b63d5176f4ab6e2958
Javier <javierf.morenor@gmail.com>
2021-10-08 08:43:33 -0500

    Rutas Perfil estudiante creadas / Falta definir POST Y GET

--- app.py
+++ app.py
@@ -4,15 +4,44 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     return render_template('Login.html')
+
+
 @app.route('/plantilla')
 def layout():
-    return render_template('layout.html')
+    return render_template('layoutE.html')
 
 @app.route('/Estudiante')
 def Estudiante():
     return render_template('Estudiante/Estudiante.html')
 
 
-@app.route('/Estudiante/Asignaturas')
-def Admin2():
-    return render_template('Estudiante/Estudiante.html')
+@app.route('/Estudiante/Cursos')
+def Opcion1():
+    return render_template('Estudiante/Cursos.html')
+
+@app.route('/Estudiante/activiades')
+def Opion2():
+    return render_template('Estudiante/actividades.html.html')
+
+@app.route('/Estudiante/entregas')
+def Opion3():
+    return render_template('Estudiante/entregas.html')
+
+@app.route('/Estudiante/perfil')
+def Opion4():
+    return render_template('Estudiante/perfil.html')
+
+@app.route('/Estudiante/asignaturas')
+def Opion5():
+    return render_template('Estudiante/asignaturas.html')
+
+@app.route('/Estudiante/novedades')
+def Opion6():
+    return render_template('Estudiante/novedades.html')
+
+
+
+@app.route('/Estudiante/CerrarSesion')
+def Opion7():
+    return render_template('Login.html')
+     
